8f12bf9c7fd0295cfb8a4d1cebd667c2a4b0e794
Jason Lind <jason.lind@groupbobaloo.com>
2018-10-27 00:04:28 -0500

    initial checkin

---
+++ L1ndus.ColabPub.Web/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L1ndus.ColabPub.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
